8d64eb641b9ac62389e16af9e84c640c5b71aaed
AA7776 <aa7776@student.jamk.fi>
2024-07-02 06:39:55 +0300

    modify user

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -3,13 +3,13 @@ import { RootState } from '../store';
 
 const UserProfile = () => {
   const user = useSelector((state: RootState) => state.users);
-
+  console.log(user)
   if (!user) return <h1>Please log in</h1>;
 
   return (
     <div>
       <h1>User Profile</h1>
-      <p>Username: {user.user}</p>
+      <p>Username: {user.user?.username}</p>
     </div>
   );
 };
